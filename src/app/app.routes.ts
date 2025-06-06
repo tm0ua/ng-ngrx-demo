baf831c94443be73475a28930bdc3a221eb41bd2
toua moua <touam@msn.com>
2025-06-06 12:34:27 -0600

    installed store and dev tools, added actions and reducer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        // Lazy load the register component.
         path: 'register',
         loadChildren: () => import('./auth/auth.routes').then(m => m.registerRoutes),
     }
--- src/app/auth/components/register/register/register.component.ts
+++ src/app/auth/components/register/register/register.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../../store/actions';
+import { RegisterRequestInterface } from '../../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.less'
 })
 export class RegisterComponent {
   public form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private store: Store) {
     this.form = this.fb.nonNullable.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
@@ -21,5 +25,9 @@ export class RegisterComponent {
 
   onSubmit() {
     console.log('form', this.form.getRawValue());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    }
+    this.store.dispatch(register({ request }))
   }
 }
